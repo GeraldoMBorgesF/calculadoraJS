b4088667f4b49c8dc428048ba7f5210999252f20
GeraldoMBorgesF <86136950+GeraldoMBorgesF@users.noreply.github.com>
2022-06-25 20:20:11 -0300

    Calculadora Java Script

---
+++ script.js
@@ -0,0 +1,20 @@
+//tipos de função
+//Declarativa
+
+// function funcao() {
+//     console.log('Sou uma função declarativa');
+// }
+// funcao();
+
+//Expressão de função com nomeação
+// var funcao = function(){
+//     console.log('Sou uma mensagem de função de expressão')
+// }
+// funcao();
+
+//arrow function
+
+// var funcao = () => {
+//     console.log( 'Sou uma arrow function!')
+// }
+// funcao();
